6b931ad995d98f6722804ee2e58117a9940fa6b8
Gaven Rendell <gaven@rendello.ca>
2021-05-14 17:15:59 -0400

    Consolidate cidr.py into monitor.py, minimal viable program

--- monitor.py
+++ monitor.py
@@ -7,18 +7,27 @@ More precisely:
     - Sandbox process with `pledge` and `unveil`.
     (Loop)
     - Read the new lines of the authlog,
-    - Grab the failed password SSH login information,
+    - Grab the failed password SSH login process_statusrmation,
     - Map the attacking IPs to their respective country codes,
     - Store the relevent attack data in the SQLite database.
     - Wait a bit; goto `Loop`.
 """
 
+import bisect
+import ipaddress
+import pathlib
+import platform
 import re
+import signal
 import sqlite3
+import sys
 import time
 
-#import openbsd
+import appdirs
+import openbsd
+import toml
 
+# Constants ===================================================================
 
 AUTHLOG_PATH = "/var/log/authlog"
 
@@ -33,7 +42,77 @@ LOG_FAILED_PASSWORD_PATTERN = re.compile(
     flags=re.MULTILINE
 )
 
+
 # =============================================================================
+
+def siginfo_handler(signal_number, frame):
+    """ See `process_status` definition """
+    global process_status
+    try:
+        print(process_status)
+    except NameError:
+        pass
+
+
+# IP-block-to-country-code functions ==========================================
+
+def file_paths_in_dir(dir_path_name):
+    file_paths = []
+    for entry in pathlib.Path(dir_path_name).iterdir():
+        if entry.is_file():
+            file_paths.append(entry)
+    return file_paths
+
+
+def pull_IP_blocks_from_dir(base_dir_path, ip_version):
+    """ Grab IP blocks per country from a data set, return a list of every
+    block's starting address in ascending order.
+    
+    Expects the base folder to have the subdirectories "ipv4" and "ipv6", each
+    of which will contain files for all countries in the format "XX.cidr",
+    where XX is the country code. The files should list the ip blocks in CIDR
+    format, one per line. """
+
+    global process_status
+
+    version_dir_path = f"{base_dir_path}/ipv{ip_version}"
+    if ip_version == 4:
+        IPNetwork = ipaddress.IPv4Network
+        IPAddress = ipaddress.IPv4Address
+    elif ip_version == 6:
+        IPNetwork = ipaddress.IPv6Network
+        IPAddress = ipaddress.IPv6Address
+
+    blocks = []
+    for path in file_paths_in_dir(version_dir_path):
+        country_code = path.name.replace(".cidr", "")
+        process_status = f'Loading IPv{ip_version} blocks for country: "{country_code.upper()}".'
+        with open(path, "r") as f:
+            for line in f.readlines():
+                ip_address = IPNetwork(line.strip()).network_address
+                blocks.append((ip_address, country_code))
+
+    process_status = f'Sorting IPv{ip_version} blocks.'
+    return sorted(blocks, key=lambda tup: IPAddress(tup[0]))
+
+
+def get_country_from_ip(ipv4_blocks, ipv6_blocks, ip):
+    """ Returns two-digit country code. """
+    if "." in ip:
+        blocks = ipv4_blocks
+        ip_tuple = (ipaddress.IPv4Address(ip), )
+    else:
+        blocks = ipv6_blocks
+        ip_tuple = (ipaddress.IPv6Address(ip), )
+
+    i = bisect.bisect_right(blocks, ip_tuple)
+    if i > len(blocks) - 1:
+        return blocks[-1][1]
+    else:
+        return blocks[i-1][1]
+
+
+# Logfile monitor functions ===================================================
 
 def convert_log_entry_timestamp(turn_over_timestamp, month_name, day_of_month, time):
     """ Change date to YYYY-MM-DDTHH:MM:SS.SSS (ISO-8601) format that's
@@ -99,13 +178,30 @@ def commit_entries_to_db(entries):
 
 # =============================================================================
 
-cursor_position = 0
-turn_over_timestamp = ""
-while True:
-    with open(AUTHLOG_PATH) as f:
-        new_entries, cursor_position, turn_over_timestamp = process_new_entries(f, turn_over_timestamp, cursor_position)
-    for e in new_entries:
-        print(e)
-    print(cursor_position)
-    print("===============")
-    time.sleep(10)
+
+
+if __name__ == "__main__":
+
+    # Information about the process' status to be returned on OpenBSD SIGINFO
+    # through `siginfo_handler()`. Should be updated when any long-running action
+    # is initiated.
+    process_status = str()
+    signal.signal(signal.SIGINFO, siginfo_handler)
+
+    config_file = pathlib.Path(appdirs.user_config_dir("SSH_Map")).joinpath("config.toml")
+    with open(config_file, "r") as f:
+        config = toml.load(f)
+
+    ipv4_blocks = pull_IP_blocks_from_dir(config["country_ip_blocks"], 4)
+    ipv6_blocks = pull_IP_blocks_from_dir(config["country_ip_blocks"], 6)
+
+    cursor_position = 0
+    turn_over_timestamp = ""
+    while True:
+        with open(AUTHLOG_PATH) as f:
+            new_entries, cursor_position, turn_over_timestamp = process_new_entries(f, turn_over_timestamp, cursor_position)
+        for e in new_entries:
+            print(e)
+            print(get_country_from_ip(ipv4_blocks, ipv6_blocks, e[2]))
+        print("===============")
+        time.sleep(10)
